d0728af8c51d5bb3e1a36c7733d8db262db6b1e5
Sean Conaty <sconaty@hearsaycorp.com>
2013-04-03 14:20:03 -0700

    add a comment to say where the custom fork lives

--- src/intro.js
+++ src/intro.js
@@ -2,6 +2,7 @@
  * <%= meta.title %> v<%= meta.version %>
  * Docs & License: <%= meta.homepage %>
  * (c) <%= meta.copyright %>
+ * This is a custom fork from https://github.com/seanconaty/fullcalendar/tree/custom
  */
 
 /*
